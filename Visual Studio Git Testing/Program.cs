ce06d1957e3d2cb34576e8126d0eccf10b32282a
ssikh <ssikh@LAPTOP-4BPBI4B2>
2022-03-20 18:09:34 -0400

    Add project files.

---
+++ Visual Studio Git Testing/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Visual_Studio_Git_Testing
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
